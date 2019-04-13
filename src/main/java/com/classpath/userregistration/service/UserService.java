178b2e06ab859fc88603046c4dda2df781c4536f
pradeep <pradeep.kumar44@gmail.com>
2019-04-14 00:17:28 +0530

    committing the changes for section-2-video-3

--- src/main/java/com/classpath/userregistration/service/UserService.java
+++ src/main/java/com/classpath/userregistration/service/UserService.java
@@ -3,12 +3,16 @@ package com.classpath.userregistration.service;
 import com.classpath.userregistration.model.User;
 import com.classpath.userregistration.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
 
 @Service
 public class UserService {
+
+    @Autowired
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
 
     @Autowired
     private UserRepository userRepository;
@@ -22,6 +26,10 @@ public class UserService {
     }
 
     public void saveUser(User user){
+        String encryptedPassword = this.bCryptPasswordEncoder.encode(user.getPassword());
+        user.setPassword(encryptedPassword);
+        user.setConfirmPassword(encryptedPassword);
+        System.out.println("************************"+user.getConfirmPassword()+" "+user.getPassword());
         this.userRepository.save(user);
     }
 }
